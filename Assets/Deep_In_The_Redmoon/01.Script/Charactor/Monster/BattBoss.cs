b9544291b6b63d225db785c811a5057b7d9de7e2
jws2170 <mpart2077@gmail.com>
2024-07-04 18:38:12 +0900

    [ADD] BatBosSkill2

--- Assets/Deep_In_The_Redmoon/01.Script/Charactor/Monster/BattBoss.cs
+++ Assets/Deep_In_The_Redmoon/01.Script/Charactor/Monster/BattBoss.cs
@@ -25,15 +25,15 @@ namespace OTO.Charactor.Monster
 
         private BatBossState batBossState;
 
-        [Header("BulletInfo")]
+        [Header("Skill")]
         [SerializeField]
         private GameObject bulletObject = null;
         [SerializeField]
-        private Transform firePos = null;
+        private GameObject fireObject = null;
         [SerializeField]
-        private float bulletSpeed = default;
+        private Transform shotPos = null;
 
-        private float rotZ = default;
+        //private variables
         private Transform playerPos = default;
 
         /// <summary>
@@ -50,7 +50,7 @@ namespace OTO.Charactor.Monster
         }
 
         /// <summary>
-        /// 
+        /// 플레이어의 위치에 따라 플립하는 코드
         /// </summary>
         protected override void Update()
         {
@@ -77,7 +77,9 @@ namespace OTO.Charactor.Monster
             rb.velocity = Vector2.down;
         }
 
-
+        /// <summary>
+        /// 보스의 행동 상태를 결정하는 함수
+        /// </summary>
         public void ChangeState()
         {
 
@@ -94,16 +96,16 @@ namespace OTO.Charactor.Monster
                         Skill1();
                         break;
                     }
-                //case BatBossState.Skill2:
-                //    {
-                //        Skill2();
-                //        break;
-                //    }
-                //case BatBossState.Skill3:
-                //    {
-                //        Skill3();
-                //        break;
-                //    }
+                case BatBossState.Skill2:
+                    {
+                        Skill2();
+                        break;
+                    }
+                    //case BatBossState.Skill3:
+                    //    {
+                    //        Skill3();
+                    //        break;
+                    //    }
             }
         }
 
@@ -116,30 +118,28 @@ namespace OTO.Charactor.Monster
             StartCoroutine(Co_Idle());
         }
 
+        /// <summary>
+        /// 기본 상태일때 실행되는 코루틴
+        /// </summary>
+        /// <returns></returns>
         private IEnumerator Co_Idle()
         {
             anim.SetTrigger("Idle");
             yield return new WaitForSeconds(3);
-            int skillIndex = 1; //Random.Range(1, 4);
+            int skillIndex = Random.Range(1, 3);
 
             switch (skillIndex)
             {
                 case 1:
                     {
                         batBossState = BatBossState.Skill1;
-                        
                         break;
                     }
-                    //case 2:
-                    //    {
-                    //        batBossState = BatBossState.Skill2;
-                    //        break;
-                    //    }
-                    //case 3:
-                    //    {
-                    //        batBossState = BatBossState.Skill3;
-                    //        break;
-                    //    }
+                case 2:
+                    {
+                        batBossState = BatBossState.Skill2;
+                        break;
+                    }
             }
 
             anim.SetTrigger("Smoke");
@@ -148,13 +148,17 @@ namespace OTO.Charactor.Monster
         }
 
         /// <summary>
-        /// 검기를 날리는 스킬을 구현한 함수
+        /// 검기를 날리는 스킬을 실행하는 함수
         /// </summary>
         private void Skill1()
         {
             StartCoroutine(Co_Skill1());
         }
 
+        /// <summary>
+        /// 검기를 날리는 로직을 구현한 코루틴
+        /// </summary>
+        /// <returns></returns>
         private IEnumerator Co_Skill1()
         {
             transform.position = new Vector2(playerPos.position.x + 15, 0.3463205f);
@@ -167,15 +171,17 @@ namespace OTO.Charactor.Monster
                 anim.SetTrigger("Skill1_Attack");
             }
 
-            anim.ResetTrigger("Skill1_Idle");
             batBossState = BatBossState.Idle;
             anim.SetTrigger("Smoke");
 
         }
 
+        /// <summary>
+        /// 검기를 날리는 함수
+        /// </summary>
         public void Skill1_Attack()
         {   
-            GameObject bullet = Instantiate(bulletObject, firePos.position, Quaternion.identity);
+            GameObject bullet = Instantiate(bulletObject, shotPos.position, Quaternion.identity);
             
             if(isFlip)
             {
@@ -189,32 +195,42 @@ namespace OTO.Charactor.Monster
             anim.SetTrigger("Skill1_Idle");
         }
 
-        ///// <summary>
-        ///// 박쥐를 소환해서 날리는 스킬을 구현한 함수
-        ///// </summary>
-        //private void Skill2()
-        //{
+        /// <summary>
+        /// 불기둥을 스킬을 실행하는 함수
+        /// </summary>
+        private void Skill2()
+        {
+            anim.SetTrigger("Skill2");
 
-        //}
+            transform.position = new Vector2(0, 10.35f);
+            StartCoroutine(Co_Skill2());
+        }
 
-        //private IEnumerator Co_Skill2()
-        //{
+        /// <summary>
+        /// 불기둥을 소환 시키는 함수
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator Co_Skill2()
+        {
+            yield return new WaitForSeconds(2);
+            int fireCount = Random.Range(1, 4);
 
-        //}
+            for(int i = 0; i < fireCount; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Instantiate(fireObject, new Vector3(50 - 10 * j, 2f, playerPos.position.z), Quaternion.identity);
+                }
+                yield return new WaitForSeconds(0.5f);
+                for (int k = 0; k < 10; k++)
+                {
+                    Instantiate(fireObject, new Vector3(45 - 10 * k, 2f, playerPos.position.z), Quaternion.identity);
+                }
+                yield return new WaitForSeconds(0.5f);
+            }
 
-        ///// <summary>
-        ///// 불기둥을 만드는 함수
-        ///// </summary>
-        //private void Skill3()
-        //{
-
-        //}
-
-        //private IEnumerator Co_Skill3()
-        //{
-
-        //}
-
-
+            batBossState = BatBossState.Idle;
+            anim.SetTrigger("Smoke");
+        }
     }
 }
---
+++ Assets/Deep_In_The_Redmoon/01.Script/Object/Fire.cs
@@ -0,0 +1,38 @@
+namespace OTO.Object
+{
+    using OTO.Charactor.Monster;
+    using OTO.Charactor.Player;
+    //System
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEditor.UIElements;
+
+    //UnityEngine
+    using UnityEngine;
+
+    public class Fire : MonoBehaviour
+    {
+        [Header("FireInfo")]
+        [SerializeField] private float fireDamage = default;
+
+        /// <summary>
+        /// 불기둥이 파괴되는 함수
+        /// </summary>
+        private void DestroyObject()
+        {
+            Destroy(gameObject);
+        }
+
+        /// <summary>
+        /// 불기둥이 특정 테그와 부딛쳤을때 실행되는 코드
+        /// </summary>
+        /// <param name="collision"></param>
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                collision.GetComponent<PlayerManager>().TakeDamage(fireDamage);
+            }
+        }
+    }
+}
